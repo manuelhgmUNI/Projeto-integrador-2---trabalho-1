85cac6fa5b6d365350478b0c74dd616f4320a094
manuelhgm <manuelmesquita.aluno@unipampa.edu.br>
2026-03-15 17:10:24 -0300

    primeio comit

---
+++ experimentos/litura_de_comandos.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    enum tipos_de_instrução
+    {
+        r = 1,
+        i = 2,
+        j = 3,
+
+    };
+    struct str_instrucao
+    {
+        enum tipos_de_instrução tipo;
+        char total[17];
+        int opcode; // 0-3
+        int rs; // 4-6
+        int rt; // 7-9
+        int rd; // 10-12
+        int funct; // 13-15
+        int immediato; // 10-15
+        int addr; //endereço do jump
+    }*instrução;
+    
+
+    FILE *Mem_ins = fopen("memoria1.mem", "r");
+}
